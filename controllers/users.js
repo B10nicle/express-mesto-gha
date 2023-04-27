233ff9ca32f9399ac89d1bb838cecb0ba3321474
Oleg Khilko <khilkoleg@gmail.com>
2023-04-27 21:12:12 +0300

    fix: users.js

--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,7 @@ module.exports.getUserById = (request, response) => {
           .send({ message: 'Bad Request' });
       }
 
-      if (err.name === 'NativeError') {
+      if (err.name === 'DocumentNotFoundError') {
         return response.status(404)
           .send({ message: 'User with _id cannot be found' });
       }
