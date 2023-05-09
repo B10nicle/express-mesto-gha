ce97ddddef341c22a2a50b6cee895369a936c6bc
Oleg Khilko <khilkoleg@gmail.com>
2023-05-09 23:43:01 +0300

    fix: bugs in validation

--- controllers/cards.js
+++ controllers/cards.js
@@ -50,7 +50,7 @@ module.exports.deleteCard = (request, response, next) => {
       if (!card.owner.equals(request.user._id)) {
         return next(new ForbiddenError('Card cannot be deleted'));
       }
-      return cardSchema.remove(card).then(() => response.send({ message: 'Card was deleted' }));
+      return card.deleteOne().then(() => response.send({ message: 'Card was deleted' }));
     })
     .catch(next);
 };
